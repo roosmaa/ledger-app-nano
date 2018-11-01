c9bd8e73837baeb67bd4f1a569dd34a8f9328fd9
Mart Roosmaa <mart@roosmaa.net>
2018-11-01 14:24:51 +0100

    Add NOS coin config

--- src/coins.c
+++ src/coins.c
@@ -66,6 +66,36 @@ REGISTER_COINS(
         /* iconToggleOn */ &C_blue_icon_toggle_on_banano,
 #endif // TARGET_BLUE
     })
+    COIN(LIBN_COIN_TYPE_NOS, {
+        /* coinName */ "NOS",
+#if defined(TARGET_BLUE)
+        /* coinBadge */ &C_blue_badge_nano,
+#else
+        /* coinBadge */ &C_nanos_badge_nano,
+#endif
+        /* bip32Prefix */ { HARDENED(44), HARDENED(229) },
+        /* addressPrimaryPrefix */ "nos_",
+        /* addressSecondaryPrefix */ "nos_",
+        /* addressDefaultPrefix */ LIBN_PRIMARY_PREFIX,
+        /* defaultUnit */ "NOS",
+        /* defaultUnitScale */ 30, // 1 Mnano = 10^30 raw
+#if defined(TARGET_BLUE)
+        /* colorBackground */ 0xF9F9F9,
+        /* colorForeground */ 0x000000,
+        /* colorAltBackground */ 0x4A90E2,
+        /* colorAltForeground */ 0xE0FFFF,
+        /* colorRejectBackground */ 0xC6C6C6,
+        /* colorRejectForeground */ 0x000000,
+        /* colorRejectOverBackground */ 0xADADAD,
+        /* colorRejectOverForeground */ 0x000000,
+        /* colorConfirmBackground */ 0x4A90E2,
+        /* colorConfirmForeground */ 0xE0FFFF,
+        /* colorConfirmOverBackground */ 0x3177C9,
+        /* colorConfirmOverForeground */ 0xFFFFFF,
+        /* iconToggleOff */ &C_blue_icon_toggle_off,
+        /* iconToggleOn */ &C_blue_icon_toggle_on_nos,
+#endif // TARGET_BLUE
+    })
 )
 
 #endif
